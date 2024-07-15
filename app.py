ffda4c7974f4ff3eb48f30a01a3aba55e1a864e1
kiruthhik <kiruthhik@gmail.com>
2024-07-15 11:44:45 +0530

    app.py remove main

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+#if __name__ == '__main__':
+ #   app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
 
     
